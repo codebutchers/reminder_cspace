ba700c0b0d9ff4428b8bbae12975f0ca14dcf7bc
TwanLee <dthuanle.20@gmail.com>
2022-06-14 22:05:57 +0700

    CBS-108: check not-reviewed reservations

--- src/task.ts
+++ src/task.ts
@@ -1,18 +1,19 @@
 import { Reservation } from "./entities";
-import { Between } from "typeorm";
-import { PAYMENT_METHOD, RESERVATION_STATUS } from "./constants";
+import { Between, LessThan } from "typeorm";
+import { RESERVATION_STATUS } from "./constants";
 import NotificationHelper from "./utils/notification";
 
 export default async function task() {
   const now = new Date();
   const tomorrow = new Date(+now + 24 * 60 * 60 * 1000);
-  const reservations = await Reservation.find({
+  // Upcoming reservations
+  const upcomingReservations = await Reservation.find({
     where: {
       fromDate: Between(now, tomorrow),
     },
     relations: ["user"],
   });
-  reservations.forEach((r) => {
+  upcomingReservations.forEach((r) => {
     if (r.user?.firebaseToken) {
       if (r.status === RESERVATION_STATUS.Paid) {
         NotificationHelper.sendToDevice(r.user.firebaseToken, {
@@ -24,10 +25,7 @@ export default async function task() {
             reservationId: `${r.id}`,
           },
         });
-      } else if (
-        r.status === RESERVATION_STATUS.Unpaid &&
-        r.paymentMethod === PAYMENT_METHOD.Paypal
-      ) {
+      } else if (r.status === RESERVATION_STATUS.Unpaid) {
         NotificationHelper.sendToDevice(r.user.firebaseToken, {
           notification: {
             title: "CBS Reminder",
@@ -38,6 +36,29 @@ export default async function task() {
           },
         });
       }
+    }
+  });
+
+  // Not reviewed reservations
+  const notReviewedReservations = await Reservation.find({
+    where: {
+      toDate: LessThan(now),
+      status: RESERVATION_STATUS.Paid,
+      hasReviewed: false,
+    },
+    relations: ["user"],
+  });
+  notReviewedReservations.forEach((r) => {
+    if (r.user?.firebaseToken) {
+      NotificationHelper.sendToDevice(r.user.firebaseToken, {
+        notification: {
+          title: "CBS Reminder",
+          body: "What do you think about our service? Please leave a review!",
+        },
+        data: {
+          reservationId: `${r.id}`,
+        },
+      });
     }
   });
 }
