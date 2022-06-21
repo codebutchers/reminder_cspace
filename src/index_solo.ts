c788954d90f8bf5d8aa251709bc3da0632bc22f1
lokishengoku <dthuanle.20@gmail.com>
2022-06-21 22:58:32 +0700

    change to every minute

--- src/index_solo.ts
+++ src/index_solo.ts
@@ -1,3 +1,23 @@
+require("dotenv").config();
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
+import "reflect-metadata";
+import { createConnection } from "typeorm";
+import { Reservation, User } from "./entities";
 import task from "./task";
 
-task();
+const main = async () => {
+  //SECTION: CONNECT POSTGRESQL
+  console.log(">>> Connecting postgreSQL");
+  await createConnection({
+    type: "postgres",
+    ssl: true,
+    url: process.env.DB_URL_PROD,
+    logging: false,
+    synchronize: false,
+    entities: [Reservation, User],
+  });
+
+  await task();
+};
+
+main().catch((err) => console.error(err));
--- src/utils/reminder.ts
+++ src/utils/reminder.ts
@@ -4,9 +4,10 @@ export default class Reminder {
   static _task: cron.ScheduledTask;
 
   static init = (task: Function) => {
-    // Reminder._task = cron.schedule("1 * * * * *", async () => { // run at 1st seconds every minute
-    // run every day at 9AM
-    Reminder._task = cron.schedule("* * 9 * * *", async () => {
+    Reminder._task = cron.schedule("1 * * * * *", async () => {
+      // run at 1st seconds every minute
+      // run every day at 9AM
+      // Reminder._task = cron.schedule("* * 9 * * *", async () => {
       await task();
     });
   };
