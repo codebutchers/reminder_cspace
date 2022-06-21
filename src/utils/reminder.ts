3aff7127cc990bc18558137d833f37b21c663b2e
lokishengoku <dthuanle.20@gmail.com>
2022-06-21 22:12:56 +0700

    CBS-108: change cron time

--- src/utils/reminder.ts
+++ src/utils/reminder.ts
@@ -4,9 +4,9 @@ export default class Reminder {
   static _task: cron.ScheduledTask;
 
   static init = (task: Function) => {
-    // Reminder._task = cron.schedule("1 * * * * *", async () => { // run every first second
-    // run at 1h every day
-    Reminder._task = cron.schedule("* * 1 * * *", async () => {
+    // Reminder._task = cron.schedule("1 * * * * *", async () => { // run at 1st seconds every minute
+    // run every day at 9AM
+    Reminder._task = cron.schedule("* * 9 * * *", async () => {
       await task();
     });
   };
