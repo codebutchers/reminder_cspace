f78b5efefb0df212ae26aad2180481590664a7bb
lokishengoku <dthuanle.20@gmail.com>
2022-06-22 21:04:16 +0700

    change to every minute

--- src/utils/reminder.ts
+++ src/utils/reminder.ts
@@ -4,10 +4,10 @@ export default class Reminder {
   static _task: cron.ScheduledTask;
 
   static init = (task: Function) => {
-    // Reminder._task = cron.schedule("1 * * * * *", async () => {
-    // run at 1st seconds every minute
-    // run every day at 9AM+7
-    Reminder._task = cron.schedule("1 * 2 * * *", async () => {
+    Reminder._task = cron.schedule("1 * * * * *", async () => {
+      // run at 1st seconds every minute
+      // run every day at 9AM+7
+      // Reminder._task = cron.schedule("0 0 2 * * *", async () => {
       await task();
     });
   };
